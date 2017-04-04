88e7d1b47bc9a285fc351bba1632d2f799cf7fad
Jorge Jimenez <george@shocklogic.com>
2017-04-04 17:32:33 -0400

    Added delete functionaliy

--- src/pages/workout-details/workout-details.ts
+++ src/pages/workout-details/workout-details.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+
+import { WorkoutsPage } from '../workouts/workouts';
+import { Workout } from '../../providers/workout';
 
 @Component({
   selector: 'page-workout-details',
-  templateUrl: 'workout-details.html'
+  templateUrl: 'workout-details.html',
+  providers: [Workout]
 })
 export class WorkoutDetailsPage {
   public workout: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public workoutProvider: Workout,
+    private loadingCtrl: LoadingController) {
     this.workout = this.navParams.get('workout');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WorkoutDetailsPage');
+  }
+
+  deleteWorkout(workoutId){
+    let loading = this.loadingCtrl.create();
+    loading.present();
+
+    var result: any;
+    this.workoutProvider.deleteWorkout(workoutId).subscribe(data => {
+      result = data;
+      loading.dismiss()
+      this.navCtrl.setRoot(WorkoutsPage);
+    }, 
+    err => console.log(err), 
+    () => console.log('Workout Deleted'));
+   
   }
 
 }
